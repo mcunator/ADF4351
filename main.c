82e41b9fe97a67dd606b76f39916e28f17bba56a
mcunator <87765170+mcunator@users.noreply.github.com>
2023-05-30 18:59:31 +0800

    Update main.c
    Add test for 100Mhz oscillator

--- main.c
+++ main.c
@@ -17,18 +17,35 @@ int main(void)
 {
   static uint32_t reg0, reg1;  // static... Avoid compiler optimisation for checking values with debugger
 	double calcfreq;
+	#define ADF_REGISTERS_COUNT 	6
+	#define RFINfreq    	100 //MHz
+	#define RFout		1575.42 //MHz GPS central
 	
 	ADF4351_ClearRegisterBuf();
 	ADF4351_Init();
-	// Just testing - some initial values
-	ADF4351_Reg2.b.RCountVal = 1;
-	ADF4351_Reg2.b.RDiv2 = ADF4351_REFDIV_2;
+	ADF4351_Reg1.b.Prescaler = ADF4351_PRESCALER_8_9;
+
+	ADF4351_Reg2.b.RCountVal = 4;
+	ADF4351_Reg2.b.CPCurrent = ADF4351_CPCURRENT_2_50;
+	ADF4351_Reg2.b.RDiv2 = ADF4351_REFDIV_1;
 	ADF4351_Reg2.b.RMul2 = ADF4351_REFMUL_1;
-	ADF4351_Reg4.b.Feedback = ADF4351_FEEDBACK_FUNDAMENTAL;
-	
-	UpdateFrequencyRegisters(1234000000.0, 50000000.0, 12500, 1, 1, &calcfreq);
-	reg0 = ADF4351_GetRegisterBuf(0);
-	reg1 = ADF4351_GetRegisterBuf(1);
+	ADF4351_Reg2.b.PhasePolarity = ADF4351_POLARITY_POSITIVE;
+
+	ADF4351_Reg4.b.OutEnable = 1;
+	ADF4351_Reg4.b.Feedback = ADF4351_FEEDBACK_DIVIDED;
+	ADF4351_Reg4.b.OutPower = ADF4351_POWER_MINUS4DB;
+
+	ADF4351_Reg5.b.LdPinMode = ADF4351_LD_PIN_DIGITAL_LOCK;
+
+	UpdateFrequencyRegisters(RFout * 1000000.0, RFINfreq * 1000000.0, 10000.0, 0, 1, &calcfreq);
+	uint32_t registers[ADF_REGISTERS_COUNT] = { 
+		ADF4351_Reg0.w, 
+		ADF4351_Reg1.w, 
+		ADF4351_Reg2.w, 
+		ADF4351_Reg3.w, 
+		ADF4351_Reg4.w, 
+		ADF4351_Reg5.w
+	};
 	
 	while (1);
 	
